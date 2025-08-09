c0d6d05c85c9506c7c45a9afa6ea9c3ed9afdb9c
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-08-10 01:37:19 +0600

    Add files via upload

---
+++ Pattern Printing/Double_hill.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Double_hill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+        for(int i=1;i<=number;i++){
+            for(int j=i;j<=number;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            for(int j=i;j<=number;j++){
+                System.out.print("  ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

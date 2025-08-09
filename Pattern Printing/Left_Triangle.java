39b5f36b15df5ffdb2e831856a8b5c6174f674fb
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-08-10 01:38:31 +0600

    Add files via upload

---
+++ Pattern Printing/Left_Triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Left_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+        for(int i=1;i<=number;i++){
+            for(int j=i;j<=number;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

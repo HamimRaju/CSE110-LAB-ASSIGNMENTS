b4f779b566b84e577144498cb108390ce3e5f2e2
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-08-11 21:18:25 +0600

    Add files via upload

---
+++ Pattern Printing/Right_triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Right_triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+        for(int i=1;i<=number;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int k=i;k<=number;k++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

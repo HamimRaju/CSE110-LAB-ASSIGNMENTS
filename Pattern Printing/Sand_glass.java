dce088be6bdbebcbe8ec17864c210675f5ce4959
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-08-11 21:18:44 +0600

    Different Pattern Printing (Using for Loop)

---
+++ Pattern Printing/Sand_glass.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sand_glass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = sc.nextInt();
+
+        for (int i = 1; i < number; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= number; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= number; i++) {
+            for (int j = i; j <= number; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

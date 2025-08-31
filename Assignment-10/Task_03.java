3cf9eb4eba87180deacd95d1d8787f66828b558c
Hamim Islam Raju <119632722+HamimRaju@users.noreply.github.com>
2025-08-31 20:18:59 +0600

    Add files via upload

---
+++ Assignment-10/Task_03.java
@@ -0,0 +1,14 @@
+public class Task_03 {
+    public static void main(String[] args) {
+        String [] email_list = {"abc@gmail.com", "!@cv.bd",  "123cse@bracu.ac.bd"};
+        int valid_email =0;
+        for(int i=0;i<email_list.length;i++){
+            String email = email_list[i];
+            if(email.contains("@")){
+                if(atIndex>0){
+                    
+                }
+            }
+        }
+    }
+}
